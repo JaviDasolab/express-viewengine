d541fec8c450450f69081e331395117e5de96be6
Javivm <javier.v.m.1994@gmail.com>
2022-11-18 16:20:30 -0800

    debug fixed, able to recieve data from form

--- index.js
+++ index.js
@@ -15,13 +15,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/tabla', (req, res) => {
-  console.log(req);
-  res.render(__dirname + '/views/tabla', {
-    titulo1: 'hola',
-    titulo2: 'adios',
-    dato1: 'dato1',
-    dato2: 'dato2'
-  })
+  res.render(__dirname + '/views/tabla', req.query)
 })
 
 app.listen(port, () => {
